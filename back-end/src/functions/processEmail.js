a4f6a30504d460106c4f065e8ed0f42fecbb7259
Ryan Earl <ryanleoearl@gmail.com>
2023-10-20 22:32:29 -0700

    Added email handling lambda

---
+++ back-end/src/functions/processEmail.js
@@ -0,0 +1,38 @@
+const aws = require("aws-sdk");
+const ses = new aws.SES({ region: "us-west-2" });
+const response = {
+  isBase64Encoded: false,
+  headers: {'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*', "Access-Control-Allow-Credentials": true}
+};
+exports.handler = async function (event) {
+  try {
+    console.log('EVENT: ', event)
+    console.log(`Body: ${event.body}`)
+    const { senderEmail, senderName, message } = JSON.parse(event.body)
+    console.log(`Email: ${senderEmail} \n Name: ${senderName} \n Message: ${message}`)
+    const params = {
+      Destination: {
+        ToAddresses: ["104herefishy@comcast.net"],
+      },
+      Message: {
+        Body: {
+          Text: { 
+              Data: `You just got a message from ${senderName} - ${senderEmail}:
+              ${message}` 
+          },
+        },
+        Subject: { Data: `Message from ${senderName}` },
+      },
+      Source: "garyhworkshopwebsite@gmail.com",
+    };
+    const sesReponse = await ses.sendEmail(params).promise();
+    console.log(sesReponse);
+    response.statusCode = 200;
+    response.body = JSON.stringify({ message: 'Email sent successfully' });
+  } catch (error) {
+    console.error(error);
+    response.statusCode = 500;
+    response.body = JSON.stringify({message: 'An error occurred while attempting to send the email'});
+  }
+  return response;
+};
